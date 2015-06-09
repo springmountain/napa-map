440cf32ab4aa8cfede1ccf13b671002f7636acc9
Stephen Orr <storr@storrdev.com>
2015-06-09 14:59:26 -0700

    Fixed viewBox on svg so that napa map fills the svg viewport

--- js/map.js
+++ js/map.js
@@ -2,10 +2,15 @@ window.onload = function() {
   var width = this.innerWidth;
   var height = this.innerHeight;
 
+  var zoom = d3.behavior.zoom()
+    .translate( [ width / 2, height / 2 ] )
+    .scale( 100 );
+
   var svg = d3.select("#vis")
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("viewBox", "1000 0 1000 2000");
 
     d3.json("data/topojson/out.json", function(error, land) {
 
@@ -13,6 +18,8 @@ window.onload = function() {
 
     svg.append("path")
       .datum(topojson.feature(land, land.objects.ava_Napa_County))
-      .attr("d", d3.geo.path().projection(null));
+      .attr("d", d3.geo.path().projection(null))
+      .attr("fill", "green")
+      .call(zoom);
   });
 };
