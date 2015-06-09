796b93457ca1e710b50825f382e5ae3a78864081
Stephen Orr <storr@storrdev.com>
2015-06-09 15:05:52 -0700

    Even more adjusting for the napa county svg

--- js/map.js
+++ js/map.js
@@ -1,6 +1,9 @@
 window.onload = function() {
-  var width = this.innerWidth;
-  var height = this.innerHeight;
+  // var width = this.innerWidth;
+  // var height = this.innerHeight;
+
+  var width = 1200;
+  var height = 1600;
 
   var zoom = d3.behavior.zoom()
     .translate( [ width / 2, height / 2 ] )
@@ -10,7 +13,8 @@ window.onload = function() {
     .append("svg")
     .attr("width", width)
     .attr("height", height)
-    .attr("viewBox", "1000 0 1000 2000");
+    .attr("viewBox", "1000 0 1010 1800")
+    .attr("preserveAspectRatio", "xMidYmid meet");
 
     d3.json("data/topojson/out.json", function(error, land) {
 
